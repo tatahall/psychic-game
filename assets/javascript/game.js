a123a8df110a9a72d7f7706cc221595d1a299c12
Tahnee Hall <tnhall2012@hotmai.com>
2019-08-28 18:19:22 -0400

    updated game to correct js so game works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,66 @@
 //create variable for computer guess and random letter generator
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 console.log(letters)
+
+//create a variable for computer's guess
+var computerGuess;
 var computerGuess = letters[Math.floor(Math.random() * letters.length)];
-var computerGuesslower = computerGuess.toLowerCase;
+console.log(computerGuess)
 
-//guesses left. starting at 9.
-var remainingGuesses = 9 - 1;
-console.log(remainingGuesses)
+//create variable for user guess
 
-//create variable for user guess.
-var Guess = document.onkeyup = function (event) {
-    userGuess.textContent = event.key;
-}
-console.log(userGuess)
-
-var userGuess = document.getElementById("userGuess");
 
 //create variables for wins and losses
-var wins = +1;
-var losses = +1;
+var wins = 0;
+var lose = 0;
 
-function compare(userGuess, computerGuess) {
-if(userGuess == computerGuess){
-document.getElementById("#wins");
-document.write(wins++)
-console.log(compare)
-}
-else{
-document.getElementById("#remguess")
-document.write(remainingGuesses--)
-}
-}
+//create a variable for guesses remaining. user should have 9 guesses.
+var remainingGuess = 9;
 
-compare();
+//create variable to write the text to appropriate place on the HTML.
+var userChoiceText = document.getElementById("userchoice-text");
+var winsText = document.getElementById("wins-text");
+var loseText = document.getElementById("lose-text");
+var remainingGuessText = document.getElementById("remaining-text");
 
-function loss(){
-if(remainingGuesses === 0){
-    document.getElementById("#losses");
-    ++losses;
+var userGuess;
+
+userChoiceText.textContent = "Your Guess: " + userGuess;
+winsText.textContent = "Wins: " + wins;
+loseText.textContent = "Losses: " + lose;
+remainingGuessText.textContent = "Remaining Guesses: " + remainingGuess;
+
+//create a variable to note the guess made so far
+var totalUserGuess = [];
+
+//create an event for the user to make their choice. User should be able press any letter a-z.
+//computer has random choice made when the game starts.
+//the user enters their choice by pressing a key (a-z).
+//if the user's guess equals the computers, the number of wins needs to increase.  The game should end and reset.
+//if the users guess does not equal the computer's guess: need to decrease remaining guess, user should 
+//choose again and the user text should update with new choice.
+//want user to have 9 chances.
+
+document.onkeyup = function (event) {
+    userGuess = event.key
+
+    if (userGuess === computerGuess) {
+        wins++;
+    }
+    else {
+        remainingGuess--;
+        totalUserGuess.push(userGuess);
+    }
+    if (remainingGuess === 0) {
+        lose++;
+    }
+
+    userChoiceText.textContent = "Your Guesses so far: " + totalUserGuess;
+    winsText.textContent = "Wins: " + wins;
+    loseText.textContent = "Losses: " + lose;
+    remainingGuessText.textContent = "Remaining Guesses: " + remainingGuess;
+
+    console.log(totalUserGuess)
+    //var playAgain = confirm("Play Again?");
+    //added the text.content below so it would increment appropriately
 }
-}
-loss();
